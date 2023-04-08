8d3c994a1f376252e062b3f266e67f991edeb1b0
bavuchoko <bavuchoko@naver.com>
2023-04-09 03:18:50 +0900

    https

--- src/main/java/com/pjs/web/config/WebConfig.java
+++ src/main/java/com/pjs/web/config/WebConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -31,4 +32,11 @@ public class WebConfig implements WebMvcConfigurer {
         return multipartResolver;
     }
 
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://parkjongsu.com")
+                .allowedMethods("*");
+    }
 }
